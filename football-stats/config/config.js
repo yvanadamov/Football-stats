28402bd3e04f36190b5a91d8f453663b29e040a0
yvanadamov <ivanadamov@abv.bg>
2016-10-31 11:36:58 +0200

    v0.0.1 Moved categories to database cfg_categories collection

--- football-stats/config/config.js
+++ football-stats/config/config.js
@@ -1,7 +1,6 @@
 module.exports = {
     "db": {
-        // to do: fix
-        "connection": "mongodb://localhost/test",
+        "connection": "mongodb://admin:admin123@ds139277.mlab.com:39277/football_stats_proto",
     },
 
     "params": {
@@ -15,38 +14,5 @@ module.exports = {
     "url": {
         "website": "http://www.oddsmath.com/football/matches/",
         "match": "http://www.oddsmath.com/api/v1/live-odds.json/?"
-    },
-
-    "categories": {
-        "0"  : {"schema":["1","X","2"]},
-        "1"  : {"schema":["1X","12","X2"]},
-        "2"  : {"schema":["1","2"],},
-        "3"  : {"subcategory":{"Handicap":"+0.5"},"schema":["1","2"]},
-        "4"  : {"subcategory":{"Handicap":"0"},"schema":["1","2"]},
-        "5"  : {"subcategory":{"Handicap":"-0.5"},"schema":["1","2"]},
-        "6"  : {"subcategory":{"Total":"2.5"},"schema":["O","U"]},
-        "7"  : {"subcategory":{"Total":"1.5"},"schema":["O","U"]},
-        "8"  : {"subcategory":{"Total":"3.5"},"schema":["O","U"]},
-        "9"  : {"subcategory":{"Total":"0.5"},"schema":["O","U"]},
-        "10" : {"subcategory":{"Total":"4.5"},"schema":["O","U"]},
-        "12" : {"subcategory":{"Total":"5.5"},"schema":["O","U"]},
-        "13" : {"subcategory":{"Handicap":"+0.25"},"schema":["1","2"]},
-        "14" : {"subcategory":{"Handicap":"-0.25"},"schema":["1","2"]},
-        "15" : {"subcategory":{"Handicap":"+0.75"},"schema":["1","2"]},
-        "16" : {"subcategory":{"Handicap":"-0.75"},"schema":["1","2"]},
-        "17" : {"subcategory":{"Handicap":"+1"},"schema":["1","2"]},
-        "18" : {"subcategory":{"Handicap":"-1"},"schema":["1","2"]},
-        "19" : {"subcategory":{"Handicap":"+1.25"},"schema":["1","2"]},
-        "20" : {"subcategory":{"Handicap":"-1.25"},"schema":["1","2"]},
-        "21" : {"subcategory":{"Handicap":"-1.5"},"schema":["1","2"]},
-        "22" : {"subcategory":{"Handicap":"+1.75"},"schema":["1","2"]},
-        "23" : {"subcategory":{"Handicap":"-1.75"},"schema":["1","2"]},
-        "24" : {"subcategory":{"Handicap":"+2"},"schema":["1","2"]},
-        "25" : {"subcategory":{"Handicap":"-2"},"schema":["1","2"]},
-        "26" : {"subcategory":{"Handicap":"+2.25"},"schema":["1","2"]},
-        "27" : {"subcategory":{"Handicap":"-2.25"},"schema":["1","2"]},
-        "28" : {"subcategory":{"Handicap":"+2.5"},"schema":["1","2"]},
-        "29" : {"subcategory":{"Handicap":"-2.5"},"schema":["1","2"]},
-        "30" : {"schema":["Y","N"]}
     }
 };
